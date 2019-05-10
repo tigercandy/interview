be072eb5f16ffe47a5bab07510bfc5b23757df91
Mantis <tangchunlinit@163.com>
2019-05-10 10:41:42 +0800

    design mode

--- src/php/loopDir.php
+++ src/php/loopDir.php
@@ -10,8 +10,7 @@ function loopDir($dir)
 {
 	$files = [];
 	if (@$handle = opendir($dir)) {
-		$file = readdir($handle);
-		while (($file = readdir($handle)) !== false) {
+		while ($file = readdir($handle) !== false) {
 			if ($file != '.' && $file != '..') {
 				if (is_dir($file)) {
 					$files[$file] = loopDir($file);
@@ -23,4 +22,40 @@ function loopDir($dir)
 	}
 	closedir($handle);
 	return $files;
+}
+
+// 对于低内存，大文件/目录的情况考虑使用yield
+function loopDirYield($dir, $include_dirs = false)
+{
+	$files = [];
+	$dir = rtrim($dir, '/*');
+	if (is_readable($dir)) {
+		if (@$handle = opendir($dir)) {
+			while ($file = readdir($handle) !== false) {
+				if ($file != '.' && $file != '..') {
+					$rfile = $dir . '/' . $file;
+					if (is_dir($file)) {
+						$sub = loopDirYield($rfile, $include_dirs);
+						while ($sub->valid) {
+							yield $sub->current();
+							$sub->next();
+						}
+						if ($include_dirs) {
+							yield $rfile;
+						}
+					} else {
+						yield $rfile;
+					}
+				}
+			}
+		}
+		closedir($handle);
+	}
+}
+
+$glob = loopDirYield('/Users/tangchunlin/Documents/interview/');
+while ($glob->valid()) {
+	$filename = $glob->current();
+	echo $filename;
+	$glob->next();
 }
